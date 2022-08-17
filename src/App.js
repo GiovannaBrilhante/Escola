4d989a2ca017b6d47f5e488b241d67d2e935f04f
GIOVANNA MARIA DO CARMO BRILHANTE <u21431@LAPA14.academico.cotuca.unicamp.br>
2022-08-17 16:30:01 -0300

    projeto escola

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+import Logo from './components/templates/Logo';
+import Menu from './components/templates/Menu';
+import Footer from './components/templates/Footer';
+import Rotas from './Rotas';
+import { BrowserRouter } from 'react-router-dom';
+
+//<Logo /> importa sozinho o componente;
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Logo /> 
+      <Menu />
+      <Rotas />
+      <Footer />
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
